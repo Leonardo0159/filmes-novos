5c073ffb230c65fd16e6daa0e7280f1521744277
Leonardo <leo.sn159@gmail.com>
2023-07-28 12:59:49 -0300

    hotjar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/src/styles/globals.css'
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id='Hotjar' strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `(function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3592250,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}}></Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
